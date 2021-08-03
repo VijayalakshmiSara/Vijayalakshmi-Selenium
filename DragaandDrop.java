1a1a05f58581b69e82739d10737a6edac032d5b5
VijayalakshmiSara <58108456+VijayalakshmiSara@users.noreply.github.com>
2021-08-03 12:53:19 +0530

    New Drag and drop java program

---
+++ DragaandDrop.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragaandDrop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver4.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/drag.html");
+		WebElement from = driver.findElement(By.id("draggable"));
+		Actions builder = new Actions(driver);
+		builder.dragAndDropBy(from, 135 , 40);
+
+	}
+
+}
